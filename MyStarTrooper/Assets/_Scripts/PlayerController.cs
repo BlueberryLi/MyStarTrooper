2488b53112b59b3b54719f43d32ee29c2d2e4016
木子 <819241959@qq.com>
2017-01-16 15:23:42 +0800

    导入资源

---
+++ MyStarTrooper/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float turnSpeed = 10.0f;
+    public float sensitivity = 1.0f;
+    public float forwardForce = 50.0f;
+    private float maxTurnLean = 50.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ MyStarTrooper/Assets/_Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+    public float rotateSpeed = 30.0f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+    }
+}
